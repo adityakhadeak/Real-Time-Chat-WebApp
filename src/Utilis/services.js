49eace2ee4d0bd410adbe9a6550eea269b1926cb
Aditya Khade <88097649+adityakhadeak@users.noreply.github.com>
2024-02-12 23:39:11 +0530

    made some changes

--- src/Utilis/services.js
+++ src/Utilis/services.js
@@ -1,5 +1,6 @@
 // export const baseUrl="http://localhost:4000"
-export const baseUrl="https://vaarta-backend.vercel.app/"
+// export const baseUrl="https://vaarta-backend.vercel.app/"
+export const baseUrl="https://vaartabackend.onrender.com/"
 export const PostRequest=async(url,body)=>{
     const response= await fetch(url,{
         method:"POST",
