5b6dd983d775da33f9e4c706ac190a179a220e4e
Aditya Khade <88097649+adityakhadeak@users.noreply.github.com>
2024-02-13 23:34:10 +0530

    Fixed some errors

--- server/index.js
+++ server/index.js
@@ -17,10 +17,10 @@ app.use('/api/user',routeUser)
 app.use('/api/chats',routeChat)
 app.use('/api/messages',routeMessages)
 
-app.use(express.static(path.join(__dirname,"/client/dist")))
+app.use(express.static(path.join(__dirname,"/client/build")))
 
 app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"client","dist","index.html"))
+    res.sendFile(path.join(__dirname,"client","build","index.html"))
 })
 ConnectToDB()
 
