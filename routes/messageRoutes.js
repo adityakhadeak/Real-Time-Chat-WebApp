f223ef88e414febc532a06abe6795fa956288aab
Aditya Khade <88097649+adityakhadeak@users.noreply.github.com>
2024-02-16 22:30:33 +0530

    Folder Structure Changed

--- routes/messageRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { createMessage, getMessages } from '../controllers/messagesController.js'
-
-const routeMessages=express()
-
-routeMessages.post('/',createMessage)
-routeMessages.get('/getmessages/:chatId',getMessages)
-
-export default routeMessages
--- src/Utilis/getUnreadNotifications.js
+++
@@ -1,8 +0,0 @@
-export const getUnreadNotifications=(notifications)=>{
-
-    const unreadNotifications=notifications?.filter((note)=>{
-        return note.isRead===false;
-    })
-
-    return unreadNotifications
-}
